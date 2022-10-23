59e2c6546bead088504234745c8a1f36f817198c
Maik Pleines <maik.pleines@googlemail.com>
2022-10-23 15:01:42 +0200

    prevent theme flickering

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,24 +1,44 @@
-import { FunctionComponent, useEffect } from 'react';
+import { FunctionComponent, useEffect, useState } from 'react';
 import { FiSun, FiMoon } from 'react-icons/fi/index.js';
-import useTheme from '../hooks/useTheme';
 
 interface ThemeToggleProps {}
 
 const ThemeToggle: FunctionComponent<ThemeToggleProps> = () => {
-  const [theme, setTheme] = useTheme();
+  const [theme, setTheme] = useState<'dark' | 'light'>(() => {
+    if (import.meta.env.SSR) {
+      return undefined;
+    }
 
-  useEffect(() => {
+    if (localStorage.getItem('theme')) {
+      return localStorage.getItem('theme');
+    }
+
+    if (window.matchMedia('prefers-color-scheme: dark').matches) {
+      return 'dark';
+    }
+
+    return 'light';
+  });
+
+  const changeTheme = (theme: 'dark' | 'light') => {
     if (theme === 'dark') {
       document.documentElement.classList.add('dark');
     } else {
       document.documentElement.classList.remove('dark');
     }
-  }, [theme]);
+
+    localStorage.setItem('theme', theme);
+    setTheme(theme);
+  };
+
+  if (theme === undefined) {
+    return <div />;
+  }
 
   return (
     <button
       className="p-2 flex items-center rounded font-bold hover:outline hover:outline-darkGrey dark:hover:outline-white"
-      onClick={() => setTheme(theme === 'dark' ? 'light' : 'dark')}
+      onClick={() => changeTheme(theme === 'dark' ? 'light' : 'dark')}
     >
       <span className="inline-flex items-center gap-1">
         {theme === 'light' ? (
